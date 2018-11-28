8e4a6bdd643cbf8bd3a2235200f4f7fc7e07a50f
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-11-28 14:48:02 -0500

    matrix convolution

--- src/mapgen.f90
+++ src/mapgen.f90
@@ -11,13 +11,23 @@ contains
   function generate_random(x,y, pulse_count, pulse_value) result(result)
     integer:: x,y, pulse_count, pulse_value
     integer, dimension(x,y) :: result
+    integer, dimension(3,3) :: kernel
 
     result = zeros(x,y)
 
     do n=1,pulse_count
-       result(randint(1,x + 1), randint(1, y+1)) = pulse_value
+       result(randint(1,x), randint(1, y)) = pulse_value
     end do
 
+    result(3,3) = 255
+
+    call print_map(result)
+
+    kernel = reshape((/0, 1, 0, 1, 4, 1, 0, 1, 0/), (/3,3/))
+    call print_map(kernel)
+
+    call convolute(result, kernel, .true., .true., 8)
+    call convolute(result, kernel, .true., .true., 8)
 
   end function
 
@@ -55,5 +65,73 @@ contains
 
   end function
 
-end module
+  subroutine convolute(array, kernel, horizontal_mirror, vertical_mirror, factor)
+    integer, dimension(:,:):: array, kernel
+    logical :: horizontal_mirror, vertical_mirror
+    integer, dimension(2) :: array_shape, kernel_shape
+    integer, pointer :: original_array(:,:)
+    integer :: factor
+
+    allocate(original_array, source=array)
+    array_shape = shape(array)
+
+    do kernel_center_row = 1, array_shape(1)
+       do kernel_center_col = 1, array_shape(2)
+          array(kernel_center_row, kernel_center_col) = kernel_sum(kernel_center_row, &
+               kernel_center_col, original_array, kernel, horizontal_mirror, vertical_mirror) / factor
+       end do
+    end do
+
+    deallocate(original_array)
+  end subroutine
+
+  integer function kernel_sum(kernel_center_row, kernel_center_col, array, kernel, horizontal_mirror, vertical_mirror)
+    integer :: kernel_center_row, kernel_center_col
+    integer, dimension(:,:) :: array, kernel
+    logical :: horizontal_mirror, vertical_mirror
+    integer, dimension(2):: kernel_shape, array_shape
+    integer :: tile_row, tile_col, row_offset, column_offset
+
+    array_shape = shape(array)
+    kernel_shape = shape(kernel)
+
+    kernel_sum = 0
+    do kernel_row=1,kernel_shape(1)
+       do kernel_column=1,kernel_shape(2)
+          row_offset = kernel_row - ceiling(kernel_shape(1) / 2.0)
+          column_offset = kernel_column - ceiling(kernel_shape(2) / 2.0)
+
+          tile_row = offset_tile(kernel_center_row, row_offset, vertical_mirror, array_shape(1))
+          tile_col = offset_tile(kernel_center_col, column_offset, horizontal_mirror, array_shape(2))
+
+          kernel_sum = kernel_sum + kernel(kernel_row, kernel_column)*array(tile_row, tile_col)
+       end do
+    end do
+  end function
+
+  integer function offset_tile(base, offset, mirror, max_value)
+    integer :: base, offset, max_value
+    logical :: mirror
+
+    offset_tile = base + offset
+
+    if (offset_tile > max_value) then
+       if (mirror) then
+          offset_tile = 2 * max_value - offset_tile
+       else
+          offset_tile = offset_tile - max_value
+       end if
+    end if
+
+    if (offset_tile < 1) then
+       if (mirror) then
+          offset_tile = 2 - offset_tile
+       else
+          offset_tile = offset_tile + max_value
+       end if
+       end if
+     end function
+
+
+   end module
 
--- src/test_mapgen.f90
+++ src/test_mapgen.f90
@@ -1,23 +1,23 @@
 program test_mapgen
   use mapgen
 
-  integer, dimension(3,4):: array
+  integer, dimension(10,8):: array
 
-  array = generate_random(3,4, 4, 255)
+  array = generate_random(10,8, 12, 180)
   ! array = zeros(3,4)
 
-  do n=1,10
-     print*, n
-  end do
+  ! do n=1,10
+  !    print*, n
+  ! end do
 
-  do i_x=1,2
-     print*, array(1:2, 1)
-  end do
+  ! do i_x=1,2
+  !    print*, array(1:2, 1)
+  ! end do
 
   print*, "print_map function"
 
   call print_map(array)
 
-  print*, randint(5,25)
+  ! print*, randint(5,25)
 
 end program
